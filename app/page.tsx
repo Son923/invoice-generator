f80ae7a9739849bbc0630956cba4d2234edee936
son923 <thaison1804@gmail.com>
2025-03-05 04:37:21 +0700

    refactor: remove ads from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import FloatingBubblesBackground from '@/components/floating-bubbles'
-import { AdBanner } from '@/components/ad-banner'
 import Link from 'next/link'
 
 export default function Home() {
@@ -61,20 +60,6 @@ export default function Home() {
               </div>
             </div>
           </Link>
-        </div>
-      </div>
-      
-      {/* Fixed position ad banner at the bottom */}
-      <div className="fixed bottom-0 left-0 right-0 z-50 p-2 bg-white/30 dark:bg-black/30 backdrop-blur-md">
-        <div className="container mx-auto max-w-5xl">
-          <AdBanner 
-            slot="9876543210" 
-            format="horizontal" 
-            height="60px"
-            backgroundColor="bg-white/70 dark:bg-gray-800/70" 
-            borderColor="border-gray-300 dark:border-gray-600"
-            className="my-0"
-          />
         </div>
       </div>
     </main>
